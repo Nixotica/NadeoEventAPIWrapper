8d6d0bb2ebb37f34dbc7c16d0e679f7911649ae3
Nixotica <nixotica@gmail.com>
2024-12-03 19:59:24 -0800

    try this for release

--- nadeo_event_api/setup.py
+++ nadeo_event_api/setup.py
@@ -1,6 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 if __name__ == "__main__":
     setup(
+        name="nadeo_event_api",
+        version="0.0.1",
+        packages=find_packages(),
         include_package_data=True,
+        package_data={
+            "nadeo_event_api.objects.outbound.pastebin": ["pastebin_2v2_template.json"],
+        },
     )

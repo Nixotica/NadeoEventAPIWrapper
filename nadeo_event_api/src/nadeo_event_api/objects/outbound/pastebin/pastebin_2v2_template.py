aabc5bc9ee4e27c5d92dca4e225b50099a62b323
Nixotica <nixotica@gmail.com>
2025-03-30 18:30:59 -0700

    switch to pastes io

--- nadeo_event_api/src/nadeo_event_api/objects/outbound/pastebin/pastebin_2v2_template.py
+++ nadeo_event_api/src/nadeo_event_api/objects/outbound/pastebin/pastebin_2v2_template.py
@@ -1,206 +1,34 @@
 PASTEBIN_TEMPLATE_DICT = [
-    {
-        "Id": "one",
+  {
+    "Id": "",
+    "Name": "",
+    "Players": [
+      {
+        "AccountId": "",
         "Name": "",
-        "LogoUrl": "",
-        "Sign2x3Url": "",
-        "Sign16x9Url": "",
-        "Sign64x10Url": "",
-        "Messages": {
-            "Ace": {
-                "Customize": [],
-                "VideoUrl": "",
-                "VideoResolution": [0, 0],
-                "VideoDelay": 0,
-                "SoundUrl": "",
-                "SoundDelay": 0,
-                "Message": "",
-                "MessageDuration": 0,
-                "MessageDelay": 0
-            },
-            "Victory": {
-                "Customize": [],
-                "VideoUrl": "",
-                "VideoResolution": [0, 0],
-                "VideoDelay": 0,
-                "SoundUrl": "",
-                "SoundDelay": 0,
-                "Message": "",
-                "MessageDuration": 0,
-                "MessageDelay": 0
-            },
-            "Draw": {
-                "Customize": [],
-                "VideoUrl": "",
-                "VideoResolution": [0, 0],
-                "VideoDelay": 0,
-                "SoundUrl": "",
-                "SoundDelay": 0,
-                "Message": "",
-                "MessageDuration": 0,
-                "MessageDelay": 0
-            },
-            "Overtime": {
-                "Customize": [],
-                "VideoUrl": "",
-                "VideoResolution": [0, 0],
-                "VideoDelay": 0,
-                "SoundUrl": "",
-                "SoundDelay": 0,
-                "Message": "",
-                "MessageDuration": 0,
-                "MessageDelay": 0
-            },
-            "TrackPoint": {
-                "Customize": ["SoundUrl", "MessageDelay"],
-                "VideoUrl": "",
-                "VideoResolution": [0, 0],
-                "VideoDelay": 0,
-                "SoundUrl": "",
-                "SoundDelay": 0,
-                "Message": "",
-                "MessageDuration": 0,
-                "MessageDelay": 700
-            },
-            "MatchPoint": {
-                "Customize": [],
-                "VideoUrl": "",
-                "VideoResolution": [0, 0],
-                "VideoDelay": 0,
-                "SoundUrl": "",
-                "SoundDelay": 0,
-                "Message": "",
-                "MessageDuration": 0,
-                "MessageDelay": 0
-            },
-            "TrackWin": {
-                "Customize": ["SoundUrl", "VideoDelay"],
-                "VideoUrl": "",
-                "VideoResolution": [0, 0],
-                "VideoDelay": 500,
-                "SoundUrl": "",
-                "SoundDelay": 0,
-                "Message": "",
-                "MessageDuration": 0,
-                "MessageDelay": 0
-            },
-            "MatchWin": {
-                "Customize": [],
-                "VideoUrl": "",
-                "VideoResolution": [0, 0],
-                "VideoDelay": 0,
-                "SoundUrl": "",
-                "SoundDelay": 0,
-                "Message": "",
-                "MessageDuration": 0,
-                "MessageDelay": 0
-            }
-        },
-        "Players": [
-            {"AccountId": "", "Name": "", "PhotoUrl": ""},
-            {"AccountId": "6e3bf3f9-7dcb-47d4-bdae-037ab66628f2", "Name": "", "PhotoUrl": ""}
-        ]
-    },
-    {
-        "Id": "two",
+        "PhotoUrl": ""
+      },
+      {
+        "AccountId": "",
         "Name": "",
-        "LogoUrl": "",
-        "Sign2x3Url": "",
-        "Sign16x9Url": "",
-        "Sign64x10Url": "",
-        "Messages": {
-            "Ace": {
-                "Customize": [],
-                "VideoUrl": "",
-                "VideoResolution": [0, 0],
-                "VideoDelay": 0,
-                "SoundUrl": "",
-                "SoundDelay": 0,
-                "Message": "",
-                "MessageDuration": 0,
-                "MessageDelay": 0
-            },
-            "Victory": {
-                "Customize": [],
-                "VideoUrl": "",
-                "VideoResolution": [0, 0],
-                "VideoDelay": 0,
-                "SoundUrl": "",
-                "SoundDelay": 0,
-                "Message": "",
-                "MessageDuration": 0,
-                "MessageDelay": 0
-            },
-            "Draw": {
-                "Customize": [],
-                "VideoUrl": "",
-                "VideoResolution": [0, 0],
-                "VideoDelay": 0,
-                "SoundUrl": "",
-                "SoundDelay": 0,
-                "Message": "",
-                "MessageDuration": 0,
-                "MessageDelay": 0
-            },
-            "Overtime": {
-                "Customize": [],
-                "VideoUrl": "",
-                "VideoResolution": [0, 0],
-                "VideoDelay": 0,
-                "SoundUrl": "",
-                "SoundDelay": 0,
-                "Message": "",
-                "MessageDuration": 0,
-                "MessageDelay": 0
-            },
-            "TrackPoint": {
-                "Customize": ["SoundUrl", "MessageDelay"],
-                "VideoUrl": "",
-                "VideoResolution": [0, 0],
-                "VideoDelay": 0,
-                "SoundUrl": "",
-                "SoundDelay": 0,
-                "Message": "",
-                "MessageDuration": 0,
-                "MessageDelay": 700
-            },
-            "MatchPoint": {
-                "Customize": [],
-                "VideoUrl": "",
-                "VideoResolution": [0, 0],
-                "VideoDelay": 0,
-                "SoundUrl": "",
-                "SoundDelay": 0,
-                "Message": "",
-                "MessageDuration": 0,
-                "MessageDelay": 0
-            },
-            "TrackWin": {
-                "Customize": ["SoundUrl", "VideoDelay"],
-                "VideoUrl": "",
-                "VideoResolution": [0, 0],
-                "VideoDelay": 500,
-                "SoundUrl": "",
-                "SoundDelay": 0,
-                "Message": "",
-                "MessageDuration": 0,
-                "MessageDelay": 0
-            },
-            "MatchWin": {
-                "Customize": [],
-                "VideoUrl": "",
-                "VideoResolution": [0, 0],
-                "VideoDelay": 0,
-                "SoundUrl": "",
-                "SoundDelay": 0,
-                "Message": "",
-                "MessageDuration": 0,
-                "MessageDelay": 0
-            }
-        },
-        "Players": [
-            {"AccountId": "", "Name": "", "PhotoUrl": ""},
-            {"AccountId": "", "Name": "", "PhotoUrl": ""}
-        ]
-    }
+        "PhotoUrl": ""
+      }
+    ]
+  },
+    {
+    "Id": "",
+    "Name": "",
+    "Players": [
+      {
+        "AccountId": "",
+        "Name": "",
+        "PhotoUrl": ""
+      },
+      {
+        "AccountId": "",
+        "Name": "",
+        "PhotoUrl": ""
+      }
+    ]
+  }
 ]
